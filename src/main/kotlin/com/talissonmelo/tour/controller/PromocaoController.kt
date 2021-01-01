c8f0bcbf299176b7ba952055995be793cb7274b8
TalissonMelo <talissonmelorodrigues@gmail.com>
2021-01-01 10:30:12 -0300

    Buscando promoção por Id

--- src/main/kotlin/com/talissonmelo/tour/controller/PromocaoController.kt
+++ src/main/kotlin/com/talissonmelo/tour/controller/PromocaoController.kt
@@ -3,6 +3,7 @@ package com.talissonmelo.tour.controller
 import com.talissonmelo.tour.entities.Promocao
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.ConcurrentHashMap
@@ -22,4 +23,7 @@ class PromocaoController {
 
     @GetMapping
     fun buscarPromocoes() = promocoes[1];
+
+    @GetMapping(value = ["/{promocaoId}"])
+    fun buscarPromocoes(@PathVariable promocaoId: Long) = promocoes[promocaoId];
 }
